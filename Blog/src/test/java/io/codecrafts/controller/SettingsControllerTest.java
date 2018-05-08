ebebce5622a021a3c61212bd657bb788a08dccee
vickyrare <vickyrare@yahoo.com>
2018-05-08 11:02:00 +1000

    add test modify profile picture in settings

--- Blog/src/test/java/io/codecrafts/controller/SettingsControllerTest.java
+++ Blog/src/test/java/io/codecrafts/controller/SettingsControllerTest.java
@@ -7,14 +7,22 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 import static org.hamcrest.Matchers.is;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
@@ -46,7 +54,7 @@ public class SettingsControllerTest {
 
     @Test
     @WithUserDetails("vickyrare@yahoo.com")
-    public void testUserTriesToViewItsOwnSettings() throws Exception{
+    public void testUserTriesToViewItsOwnSettings() throws Exception {
         this.mockMvc.perform(get("/settings"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("settings"))
@@ -58,8 +66,10 @@ public class SettingsControllerTest {
 
     @Test
     @WithUserDetails("vickyrare@yahoo.com")
-    public void testUserTriesToEditItsOwnSettings() throws Exception{
-        this.mockMvc.perform(post("/settings")
+    public void testUserTriesToEditItsOwnSettings() throws Exception {
+        MockMultipartFile mockMultipartFile = getMockMultipartFile();
+        this.mockMvc.perform(MockMvcRequestBuilders.fileUpload("/settings")
+                                     .file(mockMultipartFile)
                                      .param("firstName", "Omair")
                                      .param("lastName", "Sharif")
                                      .param("password", "12345678")
@@ -69,13 +79,16 @@ public class SettingsControllerTest {
                 .andExpect(MockMvcResultMatchers.model().attributeExists("title"))
                 .andExpect(MockMvcResultMatchers.model().attribute("title", is("Posts")))
                 .andExpect(content().string(Matchers.containsString("Omair")))
+                .andExpect(content().string(Matchers.containsString("login.jpg")))
                 .andDo(print());
     }
 
     @Test
     @WithUserDetails("vickyrare@yahoo.com")
-    public void testUserTriesToEditOtherUserSettings() throws Exception{
-        this.mockMvc.perform(post("/settings")
+    public void testUserTriesToEditOtherUserSettings() throws Exception {
+        MockMultipartFile mockMultipartFile = getMockMultipartFile();
+        this.mockMvc.perform(MockMvcRequestBuilders.fileUpload("/settings")
+                                     .file(mockMultipartFile)
                                      .param("firstName", "Omair")
                                      .param("lastName", "Sharif")
                                      .param("password", "12345678")
@@ -86,5 +99,12 @@ public class SettingsControllerTest {
                 .andExpect(MockMvcResultMatchers.model().attribute("title", is("Posts")))
                 .andExpect(content().string(Matchers.containsString("Waqqas")))
                 .andDo(print());
+    }
+
+    private MockMultipartFile getMockMultipartFile() throws IOException {
+        Resource resource = new ClassPathResource("/static/images/login.jpg");
+        File file = resource.getFile();
+        FileInputStream fileInputStream = new FileInputStream(file);
+        return new MockMultipartFile("file", file.getName(), "multipart/form-data", fileInputStream);
     }
 }
