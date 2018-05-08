f864c67e7d1fdb41f06d1bb4f013f2a94300b207
vickyrare <vickyrare@yahoo.com>
2018-05-08 11:00:41 +1000

    allow user to change profile picture in settings

--- Blog/src/main/java/io/codecrafts/controller/SettingsController.java
+++ Blog/src/main/java/io/codecrafts/controller/SettingsController.java
@@ -4,18 +4,22 @@ import io.codecrafts.model.Post;
 import io.codecrafts.model.User;
 import io.codecrafts.service.PostService;
 import io.codecrafts.service.UserService;
+import io.codecrafts.storage.StorageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 @Controller
@@ -38,9 +42,17 @@ public class SettingsController {
 		modelAndView.setViewName("settings");
 		return modelAndView;
 	}
+
+	@Autowired
+	private StorageService storageService;
+
+	@Value("${upload.file.directory}")
+	private String uploadDirectory;
 	
 	@PostMapping(value = "/settings")
-	public ModelAndView updateUser(@Valid User user, BindingResult bindingResult) {
+	public ModelAndView updateUser(@Valid User user, BindingResult bindingResult, @RequestParam("file") MultipartFile file) throws IOException {
+		Path uploadDirectoryPath = Paths.get(uploadDirectory);
+
 		ModelAndView modelAndView = new ModelAndView();
 
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
@@ -57,8 +69,20 @@ public class SettingsController {
 				updatedUser.setFirstName(user.getFirstName());
 				updatedUser.setLastName(user.getLastName());
 				updatedUser.setPassword(user.getPassword());
+				storageService.store(file);
+				String previousProfilePictureName = updatedUser.getProfilePicture();
+				updatedUser.setProfilePicture(file.getOriginalFilename());
 				userService.saveUser(updatedUser);
+				File dir = new File(uploadDirectoryPath.toFile(), updatedUser.getId().toString());
 				modelAndView.addObject("user", updatedUser);
+
+				//remove previous profile picture
+				File previousProfilePicture = new File(dir, previousProfilePictureName);
+				if (previousProfilePicture.exists()) {
+					previousProfilePicture.delete();
+				}
+
+				file.transferTo(new File(dir, file.getOriginalFilename()));
 				modelAndView.addObject("successMessage", "User settings has been changed successfully");
 			}
 
