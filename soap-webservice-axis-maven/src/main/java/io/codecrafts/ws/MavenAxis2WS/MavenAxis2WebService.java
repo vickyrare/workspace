b2c6d2c2ddc64c3e3da43ebff688ed1177fb5f83
vickyrare <vickyrare@yahoo.com>
2018-06-24 16:26:19 +1000

    add soap-webservice-axis-maven project

---
+++ soap-webservice-axis-maven/src/main/java/io/codecrafts/ws/MavenAxis2WS/MavenAxis2WebService.java
@@ -0,0 +1,10 @@
+package io.codecrafts.ws.MavenAxis2WS;
+
+public class MavenAxis2WebService {
+    public String ping(String text) {
+        if (text == null) {
+            return "Service is up and available";
+        }
+        return "Service is up and available, message: " + text;
+    }
+}
