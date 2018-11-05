87d5fe11b69f2deceb374b78d19f0a532ca833d1
VGNET\waqqas.sharif <waqqas.sharif@trapezegroup.com.au>
2018-11-06 09:59:46 +1000

    fix bug: profile images are not showing

--- Blog/src/main/java/io/codecrafts/configuration/ResourceConfig.java
+++ Blog/src/main/java/io/codecrafts/configuration/ResourceConfig.java
@@ -1,5 +1,6 @@
 package io.codecrafts.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -9,8 +10,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  */
 @Configuration
 public class ResourceConfig extends WebMvcConfigurerAdapter {
+
+    @Value("${upload.file.directory}")
+    private String uploadDirectory;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/uploads/**").addResourceLocations("file:uploads/");
+        registry.addResourceHandler("/uploads/**").addResourceLocations(uploadDirectory);
     }
 }
