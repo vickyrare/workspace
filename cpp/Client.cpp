0a182955a9f8c58be5aa096579b6adf00bcb36a7
Waqqas Sharif <vickyrare@yahoo.com>
2020-07-08 22:52:22 +1000

    add Client.cpp, Server.cpp

---
+++ cpp/Client.cpp
@@ -0,0 +1,45 @@
+//
+// Created by Waqqas Sharif on 17/4/20.
+//
+
+
+#include <iostream>
+#include <boost/array.hpp>
+#include <boost/asio.hpp>
+
+using boost::asio::ip::tcp;
+
+int main(int argc, char* argv[])
+{
+    try
+    {
+        boost::asio::io_context io_context;
+
+        tcp::resolver resolver(io_context);
+        tcp::resolver::results_type endpoints = resolver.resolve("localhost", "54000");
+
+        tcp::socket socket(io_context);
+        boost::asio::connect(socket, endpoints);
+
+        for (;;)
+        {
+            boost::array<char, 128> buf;
+            boost::system::error_code error;
+
+            size_t len = socket.read_some(boost::asio::buffer(buf), error);
+
+            if (error == boost::asio::error::eof)
+                break; // Connection closed cleanly by peer.
+            else if (error)
+                throw boost::system::system_error(error); // Some other error.
+
+            std::cout.write(buf.data(), len);
+        }
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+
+    return 0;
+}
---
+++ cpp/Server.cpp
@@ -0,0 +1,44 @@
+//
+// Created by Waqqas Sharif on 17/4/20.
+//
+
+#include <ctime>
+#include <iostream>
+#include <string>
+#include <boost/asio.hpp>
+
+using boost::asio::ip::tcp;
+
+std::string make_daytime_string()
+{
+    using namespace std; // For time_t, time and ctime;
+    time_t now = time(0);
+    return ctime(&now);
+}
+
+int main()
+{
+    try
+    {
+        boost::asio::io_context io_context;
+
+        tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), 54000));
+
+        for (;;)
+        {
+            tcp::socket socket(io_context);
+            acceptor.accept(socket);
+
+            std::string message = make_daytime_string();
+
+            boost::system::error_code ignored_error;
+            boost::asio::write(socket, boost::asio::buffer(message), ignored_error);
+        }
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+
+    return 0;
+}
