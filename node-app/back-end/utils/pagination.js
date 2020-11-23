1bb1dd6f60db7969d05d4dc324a4ae49523e8735
Waqqas Sharif <vickyrare@gmail.com>
2020-11-23 15:44:28 +1000

    add pagination.js

---
+++ node-app/back-end/utils/pagination.js
@@ -0,0 +1,10 @@
+const getPagination = (page, size) => {
+  const limit = size ? +size : 10;
+  const offset = page ? page * limit : 0;
+
+  return { limit, offset };
+};
+
+module.exports = {
+  getPagination,
+};
